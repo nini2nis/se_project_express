df27fe2419e0b7ee8d9287d055c4038990175d30
nini2nis <nini2nis@gmail.com>
2025-05-31 17:06:18 -0500

    Sprint 15 fixes

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -14,7 +14,7 @@ const getItems = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "DocumentNotFound") {
-        throw new NotFoundError("Items not found");
+        next(new NotFoundError("Items not found"));
       } else {
         next(err);
       }
@@ -30,8 +30,7 @@ const createItem = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "ValidationError") {
-        //next(new BadRequestError("Please enter a valid input")); //Dot suggestion
-        throw new BadRequestError("Please enter a valid input");
+        next(new BadRequestError("Please enter a valid input"));
       } else {
         next(err);
       }
@@ -45,7 +44,7 @@ const deleteItem = (req, res, next) => {
       if (clothingItem.owner.equals(req.user._id)) {
         return ClothingItem.findByIdAndDelete(req.params.id);
       }
-      throw new ForbiddenError("Unauthorized");
+      next(new ForbiddenError("Unauthorized"));
     })
     .then((clothingItem) => {
       res.send(clothingItem);
@@ -55,10 +54,10 @@ const deleteItem = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "DocumentNotFoundError") {
-        throw new NotFoundError("Item not found");
+        next(new NotFoundError("Item not found"));
       }
       if (err.name === "CastError") {
-        throw new BadRequestError("Invalid request");
+        next(new BadRequestError("Invalid request"));
       } else {
         next(err);
       }
@@ -92,10 +91,10 @@ const likeItem = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "DocumentNotFoundError") {
-        throw new NotFoundError("Item not found");
+        next(new NotFoundError("Item not found"));
       }
       if (err.name === "CastError") {
-        throw new BadRequestError("Invalid request");
+        next(new BadRequestError("Invalid request"));
       } else {
         next(err);
       }
@@ -115,147 +114,15 @@ const dislikeItem = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "DocumentNotFoundError") {
-        throw new NotFoundError("Item not found");
+        next(new NotFoundError("Item not found"));
       }
       if (err.name === "CastError") {
-        throw new BadRequestError("Invalid request");
+        next(new BadRequestError("Invalid request"));
       } else {
         next(err);
       }
     });
 };
-
-// const {
-//   BAD_REQUEST_STATUS,
-//   NOT_FOUND,
-//   SERVER_ERROR,
-//   FORBIDDEN,
-// } = require("../utils/errors");
-
-// const getItems = (req, res) => {
-//   ClothingItem.find({})
-//     .then((clothingItems) => res.send(clothingItems))
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "DocumentNotFound") {
-//         return res.status(NOT_FOUND).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const createItem = (req, res) => {
-//   const { name, weather, imageUrl } = req.body;
-//   ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
-//     .then((clothingItem) => res.status(201).send(clothingItem))
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "ValidationError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const deleteItem = (req, res) => {
-//   ClothingItem.findById(req.params.id)
-//     .orFail()
-//     .then((clothingItem) => {
-//       if (clothingItem.owner.equals(req.user._id)) {
-//         return ClothingItem.findByIdAndDelete(req.params.id);
-//       }
-//       return res.status(FORBIDDEN).send({ message: "Not authorized" });
-//     })
-//     .then((clothingItem) => {
-//       res.send(clothingItem);
-//     })
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "DocumentNotFoundError") {
-//         return res.status(NOT_FOUND).send({ message: err.message });
-//       }
-//       if (err.name === "CastError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const getItemById = (req, res) => {
-//   ClothingItem.findById(req.params.id)
-//     .orFail()
-//     .then((item) => {
-//       res.send(item);
-//     })
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       return res
-//         .status(SERVER_ERROR)
-//         .json({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const likeItem = (req, res) => {
-//   ClothingItem.findByIdAndUpdate(
-//     req.params.id,
-//     { $addToSet: { likes: req.user._id } },
-//     { new: true }
-//   )
-//     .orFail()
-//     .then((clothingItem) => res.send(clothingItem))
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "DocumentNotFoundError") {
-//         return res.status(NOT_FOUND).send({ message: err.message });
-//       }
-//       if (err.name === "CastError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const dislikeItem = (req, res) => {
-//   ClothingItem.findByIdAndUpdate(
-//     req.params.id,
-//     { $pull: { likes: req.user._id } },
-//     { new: true }
-//   )
-//     .orFail()
-//     .then((clothingItem) => res.send(clothingItem))
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "DocumentNotFoundError") {
-//         return res.status(NOT_FOUND).send({ message: err.message });
-//       }
-//       if (err.name === "CastError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
 
 module.exports = {
   getItems,
--- controllers/users.js
+++ controllers/users.js
@@ -22,10 +22,10 @@ const getCurrentUser = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "DocumentNotFoundError") {
-        throw new NotFoundError("User not found");
+        next(new NotFoundError("User not found"));
       }
       if (err.name === "CastError") {
-        throw new BadRequestError("Invalid data provided");
+        next(new BadRequestError("Invalid data provided"));
       } else {
         next(err);
       }
@@ -54,10 +54,10 @@ const createUser = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "ValidationError") {
-        throw new BadRequestError("Please enter a valid input");
+        next(new BadRequestError("Please enter a valid input"));
       }
       if (err.code === 11000) {
-        throw new ConflictError("Input already in use");
+        next(new ConflictError("Input already in use"));
       } else {
         next(err);
       }
@@ -80,10 +80,10 @@ const updateProfile = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.name === "ValidationError") {
-        throw new BadRequestError("Please enter a valid input");
+        next(new BadRequestError("Please enter a valid input"));
       }
       if (err.name === "DocumentNotFoundError") {
-        throw new NotFoundError("User not found");
+        next(new NotFoundError("User not found"));
       } else {
         next(err);
       }
@@ -93,7 +93,7 @@ const updateProfile = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    throw new BadRequestError("A valid email and password is required");
+    next(new BadRequestError("A valid email and password is required"));
   }
   return User.findUserByCredentials(email, password)
     .then((user) => {
@@ -107,132 +107,11 @@ const login = (req, res, next) => {
         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
       );
       if (err.message === "Incorrect email or password") {
-        throw new UnauthorizedError("Incorrect email or password");
+        next(new UnauthorizedError("Incorrect email or password"));
       } else {
         next(err);
       }
     });
 };
-
-// const {
-//   BAD_REQUEST_STATUS,
-//   NOT_FOUND,
-//   SERVER_ERROR,
-//   CONFLICT_ERROR,
-//   UNAUTHORIZED,
-// } = require("../utils/errors");
-
-// const getCurrentUser = (req, res) => {
-//   const userId = req.user._id;
-//   User.findById(userId)
-//     .orFail()
-//     .then((user) => {
-//       res.send({ user });
-//     })
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "DocumentNotFoundError") {
-//         return res.status(NOT_FOUND).send({ message: err.message });
-//       }
-//       if (err.name === "CastError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const createUser = (req, res) => {
-//   const { email, name, avatar } = req.body;
-//   bcrypt
-//     .hash(req.body.password, 10)
-//     .then((hash) =>
-//       User.create({
-//         email,
-//         password: hash,
-//         name,
-//         avatar,
-//       })
-//     )
-//     .then((user) => {
-//       const userResponse = user.toObject();
-//       delete userResponse.password;
-//       res.status(201).send(userResponse);
-//     })
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "ValidationError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       if (err.code === 11000) {
-//         return res
-//           .status(CONFLICT_ERROR)
-//           .send({ message: "Invalid: email already in use" });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const updateProfile = (req, res) => {
-//   const { name, avatar } = req.body;
-//   const update = {};
-//   if (name) update.name = name;
-//   if (avatar) update.avatar = avatar;
-//   User.findByIdAndUpdate(
-//     req.user._id,
-//     { name, avatar },
-//     { new: true, runValidators: true }
-//   )
-//     .orFail()
-//     .then((user) => res.send(user))
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.name === "ValidationError") {
-//         return res.status(BAD_REQUEST_STATUS).send({ message: err.message });
-//       }
-//       if (err.name === "DocumentNotFoundError") {
-//         return res.status(NOT_FOUND).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
-
-// const login = (req, res) => {
-//   const { email, password } = req.body;
-//   if (!email || !password) {
-//     return res
-//       .status(BAD_REQUEST_STATUS)
-//       .send({ message: "A valid email and password is required" });
-//   }
-//   return User.findUserByCredentials(email, password)
-//     .then((user) => {
-//       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
-//         expiresIn: "7d",
-//       });
-//       res.send({ token });
-//     })
-//     .catch((err) => {
-//       console.error(
-//         `Error ${err.name} with the message ${err.message} has occurred while executing the code`
-//       );
-//       if (err.message === "Incorrect email or password") {
-//         return res.status(UNAUTHORIZED).send({ message: err.message });
-//       }
-//       return res
-//         .status(SERVER_ERROR)
-//         .send({ message: "An error has occurred on the server" });
-//     });
-// };
 
 module.exports = { getCurrentUser, createUser, updateProfile, login };
